c07992de143583f9fb6602a234373ce3959131fe
Giwoong Koo <kood.d2v@gmail.com>
2023-04-28 10:58:32 +0900

    test

---
+++ src/test/java/com/laundry/laundrysample/MainTests.java
@@ -0,0 +1,12 @@
+package com.laundry.laundrysample;
+
+public class MainTests {
+    public static void main(String[] args) {
+        int a = 1, b = 16;
+
+        double result = (double) a / (double) b * 1000;
+
+        System.out.println(result);
+        System.out.println(Math.floor(result));
+    }
+}
